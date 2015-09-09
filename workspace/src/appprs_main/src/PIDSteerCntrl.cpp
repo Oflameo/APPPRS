3cf7c786a4730ae0065433160f90820d2437acc1
jgabes <jamescgabriel@gmail.com>
2015-09-09 19:09:21 -0400

    bug fix

--- workspace/src/appprs_main/src/PIDSteerCntrl.cpp
+++ workspace/src/appprs_main/src/PIDSteerCntrl.cpp
@@ -1,10 +1,12 @@
 //PID controller
 
 #include <Eigen/Dense>
-#include <math.h>
-#include "ros/ros.h"
+//#include "ros/ros.h"
 #include "std_msgs/Float32.h"
+#include "appprs_main/GoalPositionUpdater.h"
 
+//#include "tf/transform_listener.h"
+tf::TransformListener tf_listener_;
 
 
 int main(int argc, char **argv)
@@ -14,10 +16,17 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "carCommand");//start node
   ros::NodeHandle n;//create handle
   ros::Publisher chatter_pub = n.advertise<std_msgs::Float32>("carCommand", 1000);
+  
+  tf::TransformListener tf_listener_;
+
 
   float XRob_w, YRob_w, ThRob_w, Xway_w, Yway_w, Thway_w;
-  float a, b, c, d, e, f, xnew, ynew;
+  float a, b, c, d, e,f, xnew, ynew;
+  float xway_r, yway_r, Th_steer_r;
+  float Vel_r;
   ros::Rate loop_rate(50);
+
+
 while (ros::ok())
 {
 
@@ -25,11 +34,16 @@ while (ros::ok())
   tf::StampedTransform transform;
   tf_listener_.lookupTransform("/base_link", "/map", ros::Time(0), transform);
   
+  //We can do angle= tfScalar tf::Vector3::angle(transform, waypoint);
 
+  //XRob_w=transform.getOrigin().getX;
+ // YRob_w=transform.getOrigin().getX;
+//  ThRob_w=transform.getRotation().getZ();
 
-  XRob_w=transform.getOrigin().x;
-  YRob_w=transform.getOrigin().y;
-  ThRob_w=transform.getRotation().getZ();
+XRob_w=0;
+  YRob_w=0;
+  ThRob_w=0;
+
 
   //Somewhere here you get the correct waypoint coordinates in the worldframe and put them into Xway_w, Yway_w, Thway_w
   Xway_w=0;
@@ -44,8 +58,8 @@ while (ros::ok())
   f=YRob_w;
 
 
-  xnew=a*Xway_w+b*Yway_w+(-a*c-d*f)*1;
-  ynew=b*Xway_w+e*Yway_w+(-b*e-c*f)*1;
+  xway_r=a*Xway_w+b*Yway_w+(-a*c-d*f)*1;
+  yway_r=b*Xway_w+e*Yway_w+(-b*e-c*f)*1;
 
 
   if(xnew) {
