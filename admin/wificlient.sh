27edb1e9e6ac4dc2dbf928e130b78f9b33899aac
John Lewis <oflameo2@gmail.com>
2015-09-17 21:38:09 -0400

    Deletes existing ip address from wlan0 now.

--- admin/wificlient.sh
+++ admin/wificlient.sh
@@ -1,4 +1,5 @@
 # This script makes laptop ready to be a wifi clinet
 # Run this if the wifi option is greyed out
-sudo rfkill unblock wlan
+ip addr del dev wlan0
+rfkill unblock wlan
 nmcli nm wifi on
