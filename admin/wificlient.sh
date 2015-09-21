dbd0b267afdbb460921c93dbf4cc0d222a74c255
John Lewis <oflameo2@gmail.com>
2015-09-20 23:14:31 -0400

    I forgot to kill the hostapd process with the pid file.

--- admin/wificlient.sh
+++ admin/wificlient.sh
@@ -1,5 +1,6 @@
 # This script makes laptop ready to be a wifi clinet
 # Run this if the wifi option is greyed out
+kill -9 $(cat hostapd.pid)
 ip addr del dev wlan0
 rfkill unblock wlan
 nmcli nm wifi on
