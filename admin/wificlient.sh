bb747f9ed2dddff220a24549d0a7c96d45c6745f
John Lewis <oflameo2@gmail.com>
2015-09-22 19:46:34 -0400

    Quick and Dirty fix. I am removing the pid file stuff because hospad needs help creating pid files.

--- admin/wificlient.sh
+++ admin/wificlient.sh
@@ -1,6 +1,5 @@
 # This script makes laptop ready to be a wifi clinet
 # Run this if the wifi option is greyed out
-kill -9 $(cat hostapd.pid)
 ip addr del dev wlan0
 rfkill unblock wlan
 nmcli nm wifi on
