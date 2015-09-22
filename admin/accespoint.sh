3c1fcffbb63252c95f3010f59e2b88a8ccb01d2d
John Lewis <oflameo2@gmail.com>
2015-09-22 19:59:56 -0400

    Added the code to make the pid file work.

--- admin/accespoint.sh
+++ admin/accespoint.sh
@@ -1,5 +1,6 @@
 # This script turns the laptop into an access point
+touch hostapd.pid
 nmcli nm wifi off
 rfkill unblock wlan
 ip addr add 192.168.16.1/24 dev wlan0
-hostapd -B hostapd.conf
+hostapd -B -P hostapd.pid hostapd.conf
