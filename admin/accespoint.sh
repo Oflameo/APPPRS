69247f7cc7b41bb2049f3feb591282aba2f4f86d
John Lewis <oflameo2@gmail.com>
2015-09-22 21:55:34 -0400

    Giant hack of a work arround has been implimented. Things are working.

--- admin/accespoint.sh
+++ admin/accespoint.sh
@@ -1,6 +1,12 @@
+#!/bin/bash
 # This script turns the laptop into an access point
 nmcli nm wifi off
 rfkill unblock wlan
 ip addr add 192.168.16.1/24 dev wlan0
-wait 6
+sleep 5
+hostapd -B hostapd.conf
+nmcli nm wifi off
+rfkill unblock wlan
+ip addr add 192.168.16.1/24 dev wlan0
+sleep 5
 hostapd -B hostapd.conf
--- admin/wificlient.sh
+++ admin/wificlient.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 # This script makes laptop ready to be a wifi clinet
 # Run this if the wifi option is greyed out
 killall hostapd
-wait 3
+sleep 3
 ip addr del dev wlan0
 rfkill unblock wlan
 nmcli nm wifi on
