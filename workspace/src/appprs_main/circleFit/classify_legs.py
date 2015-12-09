2b4f70e816ed73184623c72295b979f684009a0b
jgabes <jamescgabriel@gmail.com>
2015-12-09 10:45:04 -0500

    updated feature finder, not yet publishing bounding boxes

--- workspace/src/appprs_main/circleFit/classify_legs.py
+++ workspace/src/appprs_main/circleFit/classify_legs.py
@@ -6,9 +6,10 @@ from sklearn.svm import LinearSVC
 import pickle
 
 def handle_classify_legs(req):
-    print "Classifying features: %s" % req.features
+    print "Classifying features: %s" % req.features[0]
     scaled_features = classifier.data_scaler.transform(req.features)
-    label = classifier.predict(scaled_features)
+    label = 1
+#classifier.predict(scaled_features)[0]
     print "Label: %s" % label
     return ClassifyLegsResponse(label)
 
@@ -19,6 +20,6 @@ def classify_legs_server():
     rospy.spin()
 
 if __name__ == "__main__":
-	with open('trained_leg_svm_classifier.pkl', 'r') as f:
-	    classifier = pickle.load(f)
+    with open('trained_leg_svm_classifier.pkl', 'r') as f:
+        classifier = pickle.load(f)
     classify_legs_server()
