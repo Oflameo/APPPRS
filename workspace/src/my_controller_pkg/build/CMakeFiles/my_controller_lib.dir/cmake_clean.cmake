cf2fb90a173115343dfc6839ff3947f53331adab
Brian Coulter <pittsburgh137@gmail.com>
2016-07-20 19:32:31 -0400

    adding draft gazebo-ros linked robot

---
+++ workspace/src/my_controller_pkg/build/CMakeFiles/my_controller_lib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/my_controller_lib.dir/src/my_controller_file.cpp.o"
+  "../lib/libmy_controller_lib.pdb"
+  "../lib/libmy_controller_lib.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/my_controller_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ workspace/src/my_controller_pkg/build/gtest/CMakeFiles/gtest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/gtest.dir/src/gtest-all.cc.o"
+  "/home/parallels/appprs/workspace/src/my_controller_pkg/lib/libgtest.pdb"
+  "/home/parallels/appprs/workspace/src/my_controller_pkg/lib/libgtest.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/gtest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
